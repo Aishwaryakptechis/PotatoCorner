0809ed4885006d7db1478f12f927863d3d9f1826
aishwaryakptechis <aishwaryakp@techis.io>
2021-11-18 09:35:15 +0530

    deployed

--- backend/apps/items/models.py
+++ backend/apps/items/models.py
@@ -8,11 +8,11 @@ class Item(models.Model):
         db_table = 'item'
 
     status = models.CharField(
-        'status', blank=False, default='inactive', max_length=15, db_index=True, choices=STATUS
+        'status', blank=False, default='inactive', max_length=50, db_index=True, choices=STATUS
     )
 
     name = models.CharField(
-        'Name', blank=False, null=False, max_length=20, db_index=True, default='Anonymous'
+        'Name', blank=False, null=False, max_length=50, db_index=True, default='Anonymous'
     )
     price = models.DecimalField(
         'price', blank=False, null=False, max_digits=14, decimal_places=2
